1d90908ce4ea15f7fbe64dcd22a649c7beeb65f2
jcmourey <jean-charles@mourey.com>
2024-05-07 20:15:08 +0200

    Initial Commit

---
+++ Banking MVVM/Banking_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Banking_MVVMApp.swift
+//  Banking MVVM
+//
+//  Created by Jean-Charles Mourey on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Banking_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
